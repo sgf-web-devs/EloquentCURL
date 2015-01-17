58f10a19de187f3142f7b43e5318698d381a0a6c
Frederick Lawler <fred@fredlawl.com>
2015-01-17 17:09:16 -0600

    Moved a test file to its proper location

--- tests/SGFWebDevs/EloquentCURL/ConnectionTest.php
+++ tests/SGFWebDevs/EloquentCURL/ConnectionTest.php
@@ -3,9 +3,8 @@
 namespace SGFWebDevs\EloquentCURL\Tests;
 
 use Mockery as M;
-use PHPUnit_Framework_TestCase;
 
-class ConnectionTest extends PHPUnit_Framework_TestCase {
+class ConnectionTest extends TestCase {
 	public function setup () {
 		parent::setUp();
 	}
