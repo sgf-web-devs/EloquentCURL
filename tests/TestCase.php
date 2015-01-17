856acb18afbd8ecf5ebe7e4574fb531370f37974
Frederick Lawler <fred@fredlawl.com>
2015-01-17 17:14:17 -0600

    Added test case php file

---
+++ tests/TestCase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace SGFWebDevs\EloquentCURL\Tests;
+
+use Mockery as M;
+use SGFWebDevs\EloquentCURL\Connection;
+use SGFWebDevs\EloquentCURL\Eloquent\Model;
+
+class TestCase extends PHPUnit_Framework_TestCase {
+
+	public function __construct () {
+		parent::__construct();
+		$this->config = require 'config/database.php';
+	}
+
+	public function setUp () {
+		parent::setUp();
+		$resolver = M::mock('Illuminate\Database\ConnectionResolverInterface');
+		$resolver->shouldReceive('connection')->andReturn($this->getConnectionWithConfig('default'));
+	}
+
+	public function tearDown () {
+		parent::tearDown();
+	}
+
+	public static function setUpBeforeClass () {
+		date_default_timezone_set('America/Chicago');
+	}
+
+	protected function getConnectionWithConfig ($config = null) {
+		$connection = array();
+		if (is_null($config))
+			$connection = $this->config['connections']['default'];
+		else
+			$connection = $this->config['connections'][$config];
+		return new Connection($connection);
+	}
+
+
+}
